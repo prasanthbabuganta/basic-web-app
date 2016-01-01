a489885d95f400fc7dc90743d87c28808d6e432f
Prasanth Babu Ganta <prasanthbabuganta@Prasanths-iMac.local>
2016-01-01 15:48:59 +0800

    Initial commit

---
+++ src/main/java/tutorial/mvc/TestController.java
@@ -0,0 +1,15 @@
+package tutorial.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by prasanthbabuganta on 1/1/16.
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "view";
+    }
+}
